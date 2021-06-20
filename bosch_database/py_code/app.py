fb80068b92429229e0c6127eee442218e4b5ef2e
ada212 <tim.schalk@haw-hamburg.de>
2021-06-20 19:36:17 +0200

    Added TTN Listener with MQTT to Docker

--- bosch_database/py_code/app.py
+++ bosch_database/py_code/app.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+print("Hier gehts noch")
+import random
+from paho.mqtt import client as mqtt_client
+import json
+import base64
+from MongoServer import MongoServer
+
+print("Main Started")
+#client = connect_mqtt()
+mongoDB = MongoServer("mongo")
+dbInputDict = {"test": "bestanden"}
+mongoDB.insert(dbInputDict)
+#subscribe(client, mongoDB)
+#client.loop_forever()
